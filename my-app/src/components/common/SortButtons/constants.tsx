49e7844b9efa5f223a78e1bef18ce6bb06434eb0
BjornHola <broadherstby@gmail.com>
2025-11-26 21:47:55 +0300

    fix: add export for type filetValues

--- my-app/src/components/common/SortButtons/constants.tsx
+++ my-app/src/components/common/SortButtons/constants.tsx
@@ -1,3 +1,5 @@
 import { type ISortButtonProps } from "./sortButton";
 
 export const sortButtonItems: ISortButtonProps["labels"] = ["Day", "Week", "Month", "Year"];
+
+export type FilterValues = "Day" | "Week" | "Month" | "Year";
